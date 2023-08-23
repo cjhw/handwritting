dfa9fd670ad1353ef6acde770892f4d411e1992e
cjhw <1784581164@qq.com>
2023-08-23 13:03:55 +0800

    feat: add bind

--- 函数节流.js
+++ 函数节流.js
@@ -1,12 +1,12 @@
 function throttle(fn, delay = 1000) {
-  let timer = 0
+  let timer = 0;
 
   return function () {
-    if (timer) return
+    if (timer) return;
 
     timer = setTimeout(() => {
-      fn.apply(this, arguments)
-      timer = 0
-    }, delay)
-  }
+      fn.apply(this, arguments);
+      timer = 0;
+    }, delay);
+  };
 }
--- 字符串模板.js
+++ 字符串模板.js
@@ -1,19 +1,19 @@
 function render(template, data) {
-  const reg = /\{\{(\w+)\}\}/
+  const reg = /\{\{(\w+)\}\}/;
   if (reg.test(template)) {
-    const name = reg.exec(template)[1]
-    console.log(reg.exec(template))
-    template = template.replace(reg, data[name])
-    return render(template, data)
+    const name = reg.exec(template)[1];
+    console.log(reg.exec(template));
+    template = template.replace(reg, data[name]);
+    return render(template, data);
   }
-  return template
+  return template;
 }
 
-let template = '我是{{name}}，年龄{{age}}，性别{{sex}}'
+let template = "我是{{name}}，年龄{{age}}，性别{{sex}}";
 let person = {
-  name: '扑街',
+  name: "扑街",
   age: 18,
-  sex: '你猜',
-}
+  sex: "你猜",
+};
 
-console.log(render(template, person))
+console.log(render(template, person));
